0b9e9455012da4273eeab7fa70261bc3eeb91351
Alexey Morozov <thecheg@yandex.ru>
2022-03-28 23:22:24 +0500

    Скролл фильтров в проектах

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -1,6 +1,6 @@
 export const server = (done) => {
 	app.plugins.browsersync.init({
-		proxy: app.path.buildFolder + '/index.html',
+		proxy: app.path.buildFolder + '/index.html', // proxy для OpenServer c доменом http://akvasmart/
 		notify: false
 	});
 }
